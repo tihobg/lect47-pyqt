a2d8cb9a9050af9c65969dbc57e2b7c8fbb588f3
Tihomir Tiankov <tiho_bg@abv.bg>
2025-03-31 17:57:30 +0300

    Commit Add New Form and Compile UI Form

--- main.py
+++ main.py
@@ -1,10 +1,17 @@
 import sys
 
 from PyQt6.QtWidgets import QApplication, QWidget, QLabel, QPushButton
+from form import Ui_Form
+
+class MyMainApp(QWidget, Ui_Form):
+    def __init__(self, parent=None):
+        super().__init__(parent)
+        self.setupUi(self)
 
 app = QApplication(sys.argv)
 
-window = QWidget()
+# window = QWidget()
+window = MyMainApp()
 window.setWindowTitle('PyQt6 App Works')
 window.setGeometry(100, 100, 500, 500)
 
